de3f38b8d512af25c3bcc2a1f7374b3e0ec96b24
Federico Bertani <fedbertani@gmail.com>
2018-02-25 22:40:52 +0100

    Slightly increased score to 929534
    but increased complexity of a n factor
    small performance and memory savings

--- pizza.py
+++ pizza.py
@@ -12,68 +12,72 @@ def getArea(start: Point, end: Point):
     return height * width
 
 
-def satifyMinIngredients(start: Point, end: Point):
-    slice = pizza[start.x:(end.x + 1), start.y:(end.y + 1)]
-    tomatoes = np.sum(slice)
-    mushrooms = np.size(slice) - tomatoes
-
+def satifyMinIngredients(pizzaSliceSum,sliceSize):
+    tomatoes = pizzaSliceSum
+    mushrooms = sliceSize - tomatoes
     if (tomatoes >= minIngredients) and (mushrooms >= minIngredients):
         return True
     else:
         return False
 
-
-def checkOverlapping(start: Point, end: Point):
-    pizzaSlice = pizza[start.x:(end.y + 1), start.y:(end.y + 1)]
-    # if there's one or more cells with NaN value, the sum is NaN
-    return math.isnan(pizzaSlice.sum())
-
-
 def visitaCella(start: Point, end: Point):
     if end.x >= rows or end.y >= cols:
         return False, start, end, 0
-    elif getArea(start, end) > maxCells or checkOverlapping(start, end):
+    elif getArea(start, end) > maxCells:
         return False, start, end, 0
-    elif satifyMinIngredients(start, end):
-        return True, start, end, getArea(start, end)
     else:
-        # recursive case
-        valid1, start1, end1, area1 = visitaCella(start, Point(x=end[0] + 1, y=end[1]))
-        valid2, start2, end2, area2 = visitaCella(start, Point(x=end[0], y=end[1] + 1))
-        # if both valid
-        if valid1 and valid2:
-            # return minimum size
-            if (area1 <= area2):
-                return True, start1, end1, area1
-            else:
-                return True, start2, end2, area2
-        elif valid1:
-            return True, start1, end1, area1
-        elif valid2:
-            return True, start2, end2, area2
-        else:
+        pizzaSlice = pizza[start.x:(end.x + 1), start.y:(end.y + 1)]
+        sliceSum = np.sum(pizzaSlice)
+        sliceSize = np.size(pizzaSlice)
+        # if there's one or more cells with NaN value, the sum is NaN
+        if math.isnan(sliceSum):
             return False, start, end, 0
+        elif satifyMinIngredients(sliceSum,sliceSize):
+            return True, start, end, getArea(start, end)
+        else:
+            # recursive case
+            valid1, start1, end1, area1 = visitaCella(start, Point(x=end[0] + 1, y=end[1]))
+            valid2, start2, end2, area2 = visitaCella(start, Point(x=end[0], y=end[1] + 1))
+            # if both valid
+            if valid1 and valid2:
+                # return minimum size
+                if (area1 <= area2):
+                    return True, start1, end1, area1
+                else:
+                    return True, start2, end2, area2
+            elif valid1:
+                return True, start1, end1, area1
+            elif valid2:
+                return True, start2, end2, area2
+            else:
+                return False, start, end, 0
 
 
-def cellVisit(firstCell: Point):
-    slices = []
+def trySlice(firstCell: Point,bestSolution):
+    solution = {}
+    solution['area'] = 0
+    solution['slices'] = []
     counter = 0
     size = rows * cols
     cycled = itertools.cycle(np.ndenumerate(pizza))
-    itertools.dropwhile(lambda index, x: (index[0] == firstCell.x) and (index[1] == firstCell.y), cycled)
     cell, value = next(cycled)
+    while not ((cell[0] == firstCell.x) and (cell[1] == firstCell.y)):
+        cell, value = next(cycled)
     while (counter <= size):
         while (math.isnan(value)):
-            counter += 1
             cell, value = next(cycled)
+            counter += 1
         cell = Point(x=cell[0], y=cell[1])
         success, start, end, area = visitaCella(cell, cell)
         if (success):
             pizza[start.x:(end.x + 1), start.y:(end.y + 1)] = None
-            slices.append({'start': start, 'end': end})
+            solution['slices'].append({'start': start, 'end': end})
+            solution['area']+=area
         cell, value = next(cycled)
-        print(str(counter * 100 / size))
-    solutions.append(slices)
+        counter += 1
+    if (solution['area']>=bestSolution['area']):
+        bestSolution['area'] = solution['area']
+        bestSolution['slices'] = solution['slices']
 
 
 def skipNan(index, x):
@@ -82,25 +86,31 @@ def skipNan(index, x):
 
 def printSolution(solution):
     with open(filename + ".out", 'w+') as outputFile:
-        outputFile.write('{}\n'.format(len(solution)))
-        for slice in solution:
+        outputFile.write('{}\n'.format(len(solution['slices'])))
+        for slice in solution['slices']:
             start = slice['start']
             end = slice['end']
             outputFile.write('{} {} {} {}\n'.format(start.x, start.y, end.x, end.y))
 
+def inizializePizza():
+    with open(filename + ".in", 'r') as inputFile:
+        # read file first line
+        line = inputFile.readline()
+        # parse parameter
+        global pizza, rows, cols, minIngredients, maxCells
+        rows, cols, minIngredients, maxCells = [int(n) for n in line.split()]
+        pizza = np.zeros([rows, cols])
+        for row in range(rows):
+            for ingredient, col in zip(inputFile.readline(), range(cols)):
+                if ingredient == 'T':
+                    pizza[row, col] = 1
+                else:
+                    pizza[row, col] = 0
 
-with open(filename + ".in", 'r') as inputFile:
-    # read file first line
-    line = inputFile.readline()
-    # parse parameter
-    rows, cols, minIngredients, maxCells = [int(n) for n in line.split()]
-    pizza = np.zeros([rows, cols])
-    for row in range(rows):
-        for ingredient, col in zip(inputFile.readline(), range(cols)):
-            if ingredient == 'T':
-                pizza[row, col] = 1
-            else:
-                pizza[row, col] = 0
-    solutions = []
-    cellVisit((0, 0))
-    printSolution(solutions[0])
+inizializePizza()
+bestSolution = {'area':0}
+for index, value in np.ndenumerate(pizza):
+    print(index)
+    trySlice(Point(x=index[0], y=index[1]), bestSolution)
+    inizializePizza()
+    printSolution(bestSolution)
